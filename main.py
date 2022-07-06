9b6a505077757dee8d4dbd74ef8891c6f4f0c1ff
Bohdan Tymanevych <bohdantymanevych@gmail.com>
2022-07-07 01:20:24 +0300

    base3

--- main.py
+++ main.py
@@ -45,14 +45,16 @@ def smerd_checker(message):
 
 	markup1 = types.InlineKeyboardMarkup(row_width=2)
 	item1 = types.InlineKeyboardButton('Я не смерд', callback_data='ne_smerd')
-	item2 = types.InlineKeyboardButton('Підсмерджую', callback_data='smerd')
+	item2 = types.InlineKeyboardButton('Підсмерджую', cacallback_data='smerd')
 	markup1.add(item1,item2)
-	bot.send_message(message.chat.id, 'Смердите?', reply_markup=markup1)
+	msg = bot.send_message(message.chat.id, 'Смердите?', reply_markup=markup1)
 	start = datetime.datetime.now()
-	end_time = start + datetime.timedelta(minutes=2)
+	end_time = start + datetime.timedelta(minutes=3)
 
 
 	sched.add_job(lambda : bot.send_message(message.chat.id, "Перекличку завершено"), trigger="cron", hour=end_time.hour, minute=end_time.minute)
+	sched.add_job(lambda : bot.edit_message_text(chat_id=msg.chat.id, message_id=msg.message_id, text='Голосування закрито',reply_markup=None)
+				  , trigger="cron", hour=end_time.hour, minute=end_time.minute)
 	sched.start()
 
 
@@ -77,7 +79,7 @@ def echo_all(message):
 def callback_status(call):
 	if call.message:
 		if call.data == 'ne_smerd':
-			print(call.message.from_user.id)
+			print(call.message.from_user.username)
 
 
 
